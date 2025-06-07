bf1321678da1946220b17f20389ac702d3cd3d04
akhilbharadwaj326 <155356933+akhilbharadwaj326@users.noreply.github.com>
2025-06-07 22:34:59 +0530

    Initial commit - Spring Boot project

--- chronos/src/main/java/com/chronos/config/SwaggerConfig.java
+++ chronos/src/main/java/com/chronos/config/SwaggerConfig.java
@@ -1,17 +1,22 @@
 package com.chronos.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "Chronos Job Scheduler API", version = "1.0"),
+        security = @SecurityRequirement(name = "bearerAuth")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT"
+)
 public class SwaggerConfig {
-    @Bean
-    public OpenAPI apiInfo() {
-        return new OpenAPI()
-                .info(new Info().title("Chronos Job Scheduler API")
-                        .description("REST API for job scheduling system")
-                        .version("1.0"));
-    }
 }
